1de511718d3d317cf5ce7898dc11d13c11efc972
Igor Doronin <ihardaronin@gmail.com>
2024-05-07 14:01:58 +0300

    add chaotic movement

--- main.py
+++ main.py
@@ -18,6 +18,10 @@ target_height = 80
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
+target_speed = 1
+dx = random.choice([-1, 1]) * target_speed
+dy = random.choice([-1, 1]) * target_speed
+
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 
@@ -27,11 +31,22 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+    target_x += dx
+    target_y += dy
+
+    if target_x <= 0 or target_x >= SCREEN_WIDTH - target_width:
+        dx *= -1
+    if target_y <= 0 or target_y >= SCREEN_HEIGHT - target_height:
+        dy *= -1
+
 
     screen.blit(target_img, (target_x, target_y))
     pygame.display.update()
